95003c1c76494b9eb7b36dae1b98e93b032f9e18
DTripleD <DTripleD@users.noreply.github.com>
2023-09-17 21:45:07 +0000

    deploy: dc51e4fe32a45971e7c70275493c61ea442d724b

---
+++ assets/CatalogPage-737542a6.js
@@ -0,0 +1,1 @@
+import{u as t,j as s}from"./index-dd7729e4.js";import{a,C as e}from"./CarsList-f58af7df.js";import"./main-car-e21e7ec8.js";const n=()=>{const r=t(a);return s.jsx(s.Fragment,{children:s.jsx(e,{cars:r})})};export{n as default};
